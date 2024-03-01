c33ba60f48b602b1aee9e37211ec73e1dd60f1f6
KaterinaBukatsich <161836973+KaterinaBukatsich@users.noreply.github.com>
2024-03-01 14:58:42 +0300

    Add files via upload

---
+++ Project/script/mobile_menu.js
@@ -0,0 +1,8 @@
+function down() {
+  var a = document.getElementById('sub-menu');
+  if ( a.style.display == 'none' )
+    a.style.display = 'block'
+  else
+    if ( a.style.display == 'block' )
+    a.style.display = 'none';
+};
